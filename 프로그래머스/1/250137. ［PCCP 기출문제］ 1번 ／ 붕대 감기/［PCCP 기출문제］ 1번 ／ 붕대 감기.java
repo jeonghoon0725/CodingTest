a333865bf89717b960cf53565761964acfc57505
jeonghoon0725 <50395417+jeonghoon0725@users.noreply.github.com>
2024-11-17 22:38:18 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 2.97 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
@@ -1,43 +1,34 @@
 import java.util.HashMap;
 class Solution {
     public int solution(int[] bandage, int health, int[][] attacks) {
-        int answer = 0;
+        int cnt = bandage[0];//추가 체력 기준
+		int now = health;//현재 체력
+		int std = 0;//마지막으로 공격당한 시간
 		
-		int ss = 0;
-		int endTime = 0;
-		int maxHealth = health;
-		int successCnt = 0;
-		HashMap<Integer, Integer> attacksMap = new HashMap<Integer, Integer>();
+		//추가 체력 받을 수 있는지
+		int v1, v2;
+		
 		for(int[] attack : attacks) {
-			attacksMap.put(attack[0], attack[1]);
-			endTime = attack[0];
-		}
-		
-		while(ss++ < endTime) {
-			if(attacksMap.containsKey(ss)) {
-				health -= attacksMap.get(ss);
-				successCnt = 0;
-			} else {
-				health += bandage[1];
-				successCnt++;
-				
-				if(successCnt == bandage[0]) {
-					health += bandage[2];
-					successCnt = 0;
-				}
-				
-				if(health > maxHealth) {
-					health = maxHealth;
-				}
-			}
-			
-			if(health <= 0) {
+			if(now <= 0) {
 				return -1;
 			}
+			
+			v1 = attack[0] - std - 1;//지금 공격당한 시간과 마지막으로 공격당한 시간 차이
+			System.out.println(attack[0]);
+			System.out.println(std);
+			
+			
+			v2 = v1 / cnt;//추가 체력 받을 수 있는지
+			
+			//맞기 직전까지 체력 정산
+			std = attack[0];
+			now = Math.min(health, now + (v1*bandage[1]));
+			now = Math.min(health, now + (v2*bandage[2]));
+			
+			//맞기
+			now -= attack[1];
 		}
 		
-		answer = health;
-		
-		return answer;
+		return now <= 0 ? -1 : now;
     }
 }
