1193a1f8c46cd38827e55b17d4afa496ce980b05
jeonghoon0725 <50395417+jeonghoon0725@users.noreply.github.com>
2024-11-25 01:06:06 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 48.05 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -1,46 +1,32 @@
 import java.util.Arrays;
 class Solution {
     public int solution(int[] diffs, int[] times, long limit) {
-        int level = 0;
+        int left = 1;
+		int right = Arrays.stream(diffs).max().getAsInt();
 		
-		int maxValue = Arrays.stream(diffs).max().getAsInt();
-		int minValue = 1;
-		
-		while(minValue <= maxValue) {
-			level = (maxValue + minValue) / 2;
+		while(left < right) {
+			int mid = (left + right) / 2;
 			
-			long result = game(level, diffs, times);
-			
-			if(limit == result) {
-				return level;
-			} else if(limit > result) {
-				long nextResult = game(level-1, diffs, times);
-				
-				if(limit < nextResult || level == 1) {
-					return level;
-				} else {
-					maxValue = level - 1;
-				}
-			} else if(limit < result) {
-				minValue = level + 1;
+			if(limit >= getTime(mid, diffs, times)) {
+				right = mid;
+			} else {
+				left = mid + 1;
 			}
 		}
 		
-		return -1;
+		return left;
 	}
 	
-	private static long game(int level, int[] diffs, int[] times) {
+	private static long getTime(int level, int[] diffs, int[] times) {
 		long time = 0;
 		
 		for(int i = 0; i < diffs.length; i++) {
-			int diff = diffs[i];
-			int time_cur = times[i];
-			int time_prev = i==0?0:times[i-1];
-			
-			if(diff <= level) {
-				time += time_cur;
+			if(level >= diffs[i]) {
+				time += times[i];
 			} else {
-				time += time_cur + (diff - level) * (time_prev + time_cur);
+				long mistakes = diffs[i] - level;
+				long prevTime = i==0?0:times[i-1];
+				time += mistakes * (prevTime + times[i]) + times[i];
 			}
 		}
 		
